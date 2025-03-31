a0a6e1cedabee671a2388e521422e23be2d107c4
danila_plater <danilaplyter@gmail.com>
2025-03-31 16:07:18 +0300

    Update HelloGit.java

--- java_core/src/main/java/ru/mentee/power/HelloGit.java
+++ java_core/src/main/java/ru/mentee/power/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("Привет, Git и GitHub!");
         System.out.println("Я начинаю изучение Java!");
+        System.out.println("Строка для создания конфлика в ветке feature_1");
     }
 }
