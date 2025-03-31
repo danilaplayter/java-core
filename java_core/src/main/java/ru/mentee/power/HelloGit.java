f02e77be037a304786799fa1c74240170fd0b9dc
danila_plater <danilaplyter@gmail.com>
2025-03-31 16:09:23 +0300

    Update HelloGit.java
    Вторая строка для конфликта

--- java_core/src/main/java/ru/mentee/power/HelloGit.java
+++ java_core/src/main/java/ru/mentee/power/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("Привет, Git и GitHub!");
         System.out.println("Я начинаю изучение Java!");
+        System.out.println("Вторая строка для создания конфликта при слиянии");
     }
 }
